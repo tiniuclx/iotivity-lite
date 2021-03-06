3c2d01ac8d5bc4ce3fbae50e39911a860d70c003
Daniel Adam <daniel.adam1922@protonmail.com>
2021-03-06 21:45:33 +0100

    cloud_server: add OC_DELETE handler to created switches

--- apps/cloud_server.c
+++ apps/cloud_server.c
@@ -325,7 +325,6 @@ get_switches_properties(oc_resource_t *resource, oc_interface_mask_t iface_mask,
   }
 }
 
-
 /* Resource creation and request handlers for oic.r.switch.binary instances */
 typedef struct oc_switch_t
 {
@@ -335,8 +334,8 @@ typedef struct oc_switch_t
   bool state;
 } oc_switch_t;
 
-
 #ifdef OC_COLLECTIONS_IF_CREATE
+
 OC_MEMB(switch_s, oc_switch_t, 1);
 OC_LIST(switches); // list of switch instances ordered by id
 
@@ -428,7 +427,27 @@ get_cswitch(oc_request_t *request, oc_interface_mask_t iface_mask,
   oc_send_response(request, OC_STATUS_OK);
 }
 
-//#ifdef OC_COLLECTIONS_IF_CREATE
+static oc_event_callback_retval_t
+delete_from_collection(void* res)
+{
+  oc_resource_t* r = (oc_resource_t*)res;
+  oc_cloud_delete_resource(r);
+  oc_delete_resource(r);
+  return OC_EVENT_DONE;
+}
+
+static void
+delete_cswitch(oc_request_t *request, oc_interface_mask_t iface_mask,
+            void *user_data)
+{
+  OC_DBG("%s", __func__);
+  (void)request;
+  (void)iface_mask;
+  oc_switch_t *cswitch = (oc_switch_t *)user_data;
+
+  oc_set_delayed_callback(cswitch->resource, delete_from_collection, 0);
+  oc_send_response(request, OC_STATUS_DELETED);
+}
 
 /**
  *  Get pointer to the first element that either has no following element
@@ -493,13 +512,14 @@ get_switch_instance(const char *href, oc_string_array_t *types,
       oc_resource_set_default_interface(cswitch->resource, OC_IF_A);
       oc_resource_set_request_handler(cswitch->resource, OC_GET, get_cswitch,
                                       cswitch);
+      oc_resource_set_request_handler(cswitch->resource, OC_DELETE, delete_cswitch,
+                                      cswitch);
       oc_resource_set_request_handler(cswitch->resource, OC_POST, post_cswitch,
                                       cswitch);
       oc_resource_set_properties_cbs(cswitch->resource, get_switch_properties,
                                      cswitch, set_switch_properties, cswitch);
       oc_add_resource(cswitch->resource);
       oc_set_delayed_callback(cswitch->resource, register_to_cloud, 0);
-
       oc_list_insert(switches, prev, cswitch);
       return cswitch->resource;
     } else {
@@ -512,6 +532,7 @@ get_switch_instance(const char *href, oc_string_array_t *types,
 static void
 free_switch_instance(oc_resource_t *resource)
 {
+  OC_DBG("%s", __func__);
   oc_switch_t *cswitch = (oc_switch_t *)oc_list_head(switches);
   while (cswitch) {
     if (cswitch->resource == resource) {
