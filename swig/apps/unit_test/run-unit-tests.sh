d5ed67cf3f5779ac23637619f2eb1aeaa3b22145
Wouter van der Beek <w.vanderbeek@cascoda.com>
2021-04-06 03:48:37 -0700

    Update swig/apps/unit_test/run-unit-tests.sh

--- swig/apps/unit_test/run-unit-tests.sh
+++ swig/apps/unit_test/run-unit-tests.sh
@@ -2,4 +2,4 @@
 
 #$HOME/junit/junit-4.13.jar
 #$HOME/junit/hamcrest-core-1.3.jar
-java -Djava.library.path=../../iotivity-lite-java/libs -cp iotivity-junit.jar:../../iotivity-lite-java/libs/iotivity-lite.jar:$JUNIT4:$HAMCREST_CORE org.iotivity.TestRunner
+# java -Djava.library.path=../../iotivity-lite-java/libs -cp iotivity-junit.jar:../../iotivity-lite-java/libs/iotivity-lite.jar:$JUNIT4:$HAMCREST_CORE org.iotivity.TestRunner
